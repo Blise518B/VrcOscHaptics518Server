e1418d7204762a1364fbdf3175787adce2b7b5e8
Blise518B <justus.kullmann@googlemail.com>
2023-06-12 21:36:10 +0200

    added 8 Seperate ESP , changed OSC Fromat

--- VrcOscHaptics518_Server.py
+++ VrcOscHaptics518_Server.py
@@ -4,12 +4,23 @@ import websockets
 import asyncio
 import threading
 
-# WebSocket server address (replace with your ESP8266 IP address)
-server_address = "ws://HapticESP-1:8080"
+# WebSocket server addresses (replace with your ESP8266 IP addresses)
+server_addresses = [
+    "ws://ESP-L-Arm:8080",
+    "ws://ESP-F-Body:8080",
+    "ws://ESP-R-Body:8080",
+    "ws://ESP-R-Arm:8080",
+    "ws://ESP-Head:8080",
+    "ws://ESP-L-Leg:8080",
+    "ws://ESP-R-Leg:8080",
+    "ws://ESP-Extra:8080",
+]
 
 # Number of boolean outputs
-num_outputs = 6
+num_outputs = 128
 
+# Number of boolean outputs per ESP
+outputs_per_esp = 16
 
 # Initialize the boolean values and previous state
 bool_values = [False] * num_outputs
@@ -18,9 +29,9 @@ prev_bool_values = bool_values.copy()
 # OSC message handler
 def handle_osc_message(address, *args):
     global bool_values
-    if address.startswith("/avatar/parameters/L_Arm"):
+    if address.startswith("/avatar/parameters/Sensor"):
         try:
-            index = int(address.split("/")[-1][len("L_Arm") : ]) - 1
+            index = int(address.split("/")[-1][len("Sensor"):]) - 1
             if 0 <= index < num_outputs:
                 if len(args) > 0 and isinstance(args[0], bool):
                     bool_values[index] = args[0]
@@ -31,31 +42,40 @@ def handle_osc_message(address, *args):
         except ValueError:
             print("Invalid address format:", address)
 
-    #print("Received OSC message:", address, args)
+    # print("Received OSC message:", address, args)
+
 
 # WebSocket client function
-async def send_data():
+async def send_data(server_address, start_index, end_index):
     global prev_bool_values  # Declare prev_bool_values as global
-    async with websockets.connect(server_address) as ws:
-        while True:
-            # Check if there is a difference between the current state and the previous state
-            if bool_values != prev_bool_values:
-                # Convert the boolean values to a string payload
-                payload = "".join("1" if value else "0" for value in bool_values)
+    try:
+        async with websockets.connect(server_address) as ws:
+            print("Connected to", server_address)
+            while True:
+                # Check if there is a difference between the current state and the previous state
+                if bool_values[start_index:end_index] != prev_bool_values[start_index:end_index]:
+                    # Convert the boolean values to a string payload
+                    payload = "".join(
+                        "1" if value else "0" for value in bool_values[start_index:end_index]
+                    )
 
-                # Send the payload to the ESP8266
-                await ws.send(payload)
+                    # Send the payload to the ESP8266
+                    await ws.send(payload)
 
-                # Print the payload sent
-                print("Sent data:", payload)
+                    # Print the payload sent
+                    print("Sent data to", server_address, ":", payload)
 
-                # Update the previous state
-                prev_bool_values = bool_values.copy()
+                    # Update the previous state
+                    prev_bool_values[start_index:end_index] = bool_values[start_index:end_index]
 
-            # Delay for 100 milliseconds
-            await asyncio.sleep(0.1)
+                # Delay for 100 milliseconds
+                await asyncio.sleep(0.1)
+    except Exception as e:
+        print("WebSocket client error:", str(e))
+        print("Failed to connect to", server_address)
 
-# Start OSC server and run the WebSocket client
+
+# Start OSC server and run the WebSocket clients
 if __name__ == "__main__":
     dispatcher = Dispatcher()
     dispatcher.set_default_handler(handle_osc_message)
@@ -73,69 +93,20 @@ if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    # Run the WebSocket client
+    # Run the WebSocket clients
+    tasks = []
+    for i, server_address in enumerate(server_addresses):
+        start_index = i * outputs_per_esp
+        end_index = start_index + outputs_per_esp
+        task = asyncio.ensure_future(send_data(server_address, start_index, end_index))
+        tasks.append(task)
+
     try:
-        loop.run_until_complete(send_data())
-    except Exception as e:
-        print("WebSocket client error:", str(e))
+        loop.run_until_complete(asyncio.gather(*tasks))
+    except KeyboardInterrupt:
+        pass
     finally:
         # Clean up resources
         server.shutdown()
         server_thread.join()
         print("OSC server stopped.")
-
-
-
-
-
-
-
-'''
-import time
-import random
-import websockets
-import asyncio
-
-# Number of boolean outputs
-num_outputs = 8
-
-# WebSocket server address (replace with your ESP8266 IP address)
-server_address = "ws://192.168.0.75:8080"
-
-async def send_data():
-    async with websockets.connect(server_address) as ws:
-        while True:
-            # Send data for 5 seconds
-            start_time = time.time()
-            while time.time() - start_time < 5:
-                # Generate a random index from 0 to num_outputs-1
-                index = random.randint(0, num_outputs - 1)
-
-                # Create a list of boolean values
-                data = ['0'] * num_outputs
-                data[index] = '1'
-
-                # Convert the list to a string
-                payload = ''.join(data)
-
-                # Send the payload to the ESP8266
-                await ws.send(payload)
-
-                # Print the payload sent
-                print("Sent data:", payload)
-
-                # Delay for 100 milliseconds
-                await asyncio.sleep(0.1)
-
-            # Stop for 5 seconds
-            await asyncio.sleep(5)
-
-# Run the WebSocket client
-asyncio.get_event_loop().run_until_complete(send_data())
-
-'''
-
-#const char* ssid = "VR-Gehirnwaescheeinheit24";
-#const char* password = "LidBd21J";
-#const char* server_ip = "192.168.0.29";
-# uri = "ws://192.168.0.90:8080"
